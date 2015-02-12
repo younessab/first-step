10f232679432b741427caa97e0597da836841209
younessab <younes.ayeb@gmail.com>
2015-02-12 11:20:26 +0100

    add ney class

---
+++ src/src/JavaClass.java
@@ -0,0 +1,11 @@
+package src;
+
+public class JavaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	     System.out.println("Generating StremC Code" ); 
+
+	}
+
+}
