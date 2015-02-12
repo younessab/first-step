e483bddbc30317ab2f53217c022578ad4874a9b1
younessab <younes.ayeb@gmail.com>
2015-02-12 11:23:00 +0100

    Update JavaClass.java

--- src/src/JavaClass.java
+++ src/src/JavaClass.java
@@ -5,7 +5,8 @@ public class JavaClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	     System.out.println("Generating StremC Code" ); 
-
+System.out.println("Generating StremC Code" ); 
+	     
 	}
 
 }
